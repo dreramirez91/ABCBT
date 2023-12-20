37b8e0f11674508c38f9c3edb36322198c3634df
dreramirez91 <dreramirez91@gmail.com>
2023-12-20 14:07:39 -0600

    Add CRUD for single disputation

--- server/wsgi.py
+++ server/wsgi.py
@@ -2,9 +2,8 @@ from flask import Flask, request
 from markupsafe import escape
 from pymongo import MongoClient
 from dotenv import dotenv_values
-from bson import json_util
+from bson import json_util, ObjectId
 import json
-import pprint
 from json import JSONEncoder
 
 
@@ -23,10 +22,13 @@ with app.app_context():
     print("\n\n\n", "Connected to MongoDB!", app.database, "\n\n\n")
     
 @app.route("/disputations/", methods=['GET', 'POST', 'DELETE'])
-async def add_disputation():
+def crud_all_disputations():
     if request.method == 'GET':
         all_disputations = app.database["disputations"].find()
         data = json.loads(json_util.dumps([d for d in all_disputations]))
+        print(type(data), "-------\n\n\n")
+        for d in data:
+            d["_id"] = d["_id"]["$oid"]
         return {"disputations": data}
     elif request.method == 'POST':
         disputation = request.json
@@ -38,6 +40,22 @@ async def add_disputation():
     elif request.method == 'DELETE':
         response = app.database["disputations"].delete_many({})
         print("\n\n\n----------", response, "---------\n\n\n")
-        return {"response": response.deleted_count}
+        return {"deleted_count": response.deleted_count}
 
+@app.route("/disputations/<id>/", methods=['GET', 'PUT', 'DELETE'])
+def crud_one_disputation(id):
+    if request.method == 'GET':
+        one_disputation = app.database["disputations"].find_one({"_id": ObjectId(id)})
+        data = json.loads(json_util.dumps(one_disputation))
+        return {"disputation": data}
+    elif request.method == 'DELETE':
+        response = app.database["disputations"].delete_one({"_id": ObjectId(id)})
+        print("\n\n\n----------", response, "---------\n\n\n")
+        return {"deleted_count": response.deleted_count}
+    elif request.method == 'PUT':
+        disputation = request.json
+        response = app.database["disputations"].replace_one({"_id": ObjectId(id)}, disputation)
+        return {"updated_count": response.modified_count}
+        
+    
 # flake8:noqa
